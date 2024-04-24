82e899cbde3408dfacdbf4cc9cd9f8fa9231ac03
sayyidnaqvi <naqvi.sdsu@gmail.com>
2024-04-24 01:59:37 -0500

    refactored, GithubReposService simplified

--- app/src/main/java/demo/gitprofiles/di/module/GitRepositoryModule.kt
+++ app/src/main/java/demo/gitprofiles/di/module/GitRepositoryModule.kt
@@ -1,19 +1,15 @@
 package demo.gitprofiles.di.module
 
-import android.content.Context
-import androidx.room.Room
 import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
 import demo.gitprofiles.BuildConfig.BASE_URL
 import demo.gitprofiles.di.GithubReposService
-import demo.gitprofiles.gitreposlist.data.local.profileDB.GitProfileDetailsDatabase
 import demo.gitprofiles.gitreposlist.data.network.GithubreposApi
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 @Module
 class GitRepositoryModule {
